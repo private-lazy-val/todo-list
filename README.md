3c0972f56ff28e8bf6450bf8525e45babd07f39f
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-06-06 13:32:14 +0400

    feat: semi-completed project

---
+++ README.md
@@ -0,0 +1,30 @@
+## Table of Contents
+
+[Overview](#overview)
+- [Hosting](#hosting)
+- [Technologies](#technologies)
+
+## Overview
+
+This is a simple To Do List web app that allows users to create tasks, group them into lists, and mark them as complete once done. Tasks can be easily added, edited, and deleted.
+Additional features include a task counter and a feature to clear completed tasks.
+
+### Hosting
+
+To do list app is hosted on GitHub Pages. You can view the live site here:
+
+- [https://private-lazy-val.github.io/todoList/](https://private-lazy-val.github.io/todoList/)
+
+### Technologies
+
+The following technologies were used to develop this project:
+
+
+- HTML5 / CSS3 / JS
+- Media queries
+- Responsive web design
+- Flexbox and Grid
+- Webpack
+- Babel
+- PostCSS
+- Core-JS
---
+++ src/pages/index.js
@@ -0,0 +1,144 @@
+const listContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListBtn = document.querySelector('[data-delete-list-btn]');
+const deleteCompleteTaskBtn = document.querySelector('[data-delete-completed-task-btn]');
+const listDisplayContainer = document.querySelector('[data-list-display-container]');
+const listTitleElement = document.querySelector('[data-list-title]');
+const listCountElement = document.querySelector('[data-list-count]');
+const tasksContainer = document.querySelector('[data-tasks]');
+const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput = document.querySelector('[data-new-task-input]');
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+})
+
+tasksContainer.addEventListener('click', e =>
+{
+  if(e.target.tagName.toLowerCase() === 'input') {
+    const selectedList = lists.find(list => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
+  }
+})
+newListForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === '') return;
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+})
+
+newTaskForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === '') return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find(list => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+})
+
+function createList(name) {
+  return {id: Date.now().toString(), name: name, tasks: []};
+}
+function createTask(name) {
+  return {id: Date.now().toString(), name: name, complete: false};
+}
+
+deleteListBtn.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
+})
+
+deleteCompleteTaskBtn.addEventListener('click', e => {
+  const selectedList = lists.find(list => list.id === selectedListId);
+  selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
+  saveAndRender();
+})
+
+// Save lists to the local storage
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
+
+// Render new list in the lists container
+function render() {
+  clearElement(listContainer);
+  renderList();
+
+  const selectedList = lists.find(list => list.id === selectedListId);
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none';
+  } else {
+    listDisplayContainer.style.display = '';
+    listTitleElement.textContent = selectedList.name;
+    renderTaskCount(selectedList);
+    clearElement(tasksContainer);
+    renderTasks(selectedList);
+  }
+}
+
+function renderTasks(selectedList) {
+  selectedList.tasks.forEach(task => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkbox = taskElement.querySelector('.todo-list__input');
+    checkbox.id = task.id;
+    checkbox.checked = task.complete;
+    const label = taskElement.querySelector('.todo-list__label');
+    label.htmlFor = task.id;
+    label.append(task.name);
+    tasksContainer.appendChild(taskElement);
+  })
+}
+
+
+function renderTaskCount(selectedList) {
+  const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length;
+  const taskString = incompleteTaskCount === 1 ? "task" : "tasks";
+  listCountElement.textContent = `${incompleteTaskCount} ${taskString} remaining`;
+}
+
+function renderList() {
+  lists.forEach(list => {
+    const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add('all-tasks__item');
+    listElement.textContent = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add('all-tasks__list-item_active');
+    }
+    listContainer.appendChild(listElement);
+  })
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+
+render();
